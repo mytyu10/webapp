1da735de6a822c6a89148c9fb320fb741e0fe719
yuto.yorozu <yuto.yorozu@gmail.com>
2026-02-28 12:50:37 +0900

    changed to APIView

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #urlが指定できていない場合と、urlの指定が「login」の場合にはlogin画面へ遷移させる。
+    path('', include('login.urls')),
+    path('login/', include('login.urls')),
 ]
--- backend/login/views.py
+++ backend/login/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+class LoginView(APIView):
+    def get(self, request: Request):
+        return Response({"message": "Hello from class"})
