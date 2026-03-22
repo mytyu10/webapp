985edec4bc875298338bb636430674d8865a6937
yuto.yorozu <yutoyoozu@gmail.com>
2026-03-22 23:09:45 +0900

    ビジネスロジックの修正とメッセージ管理用ファイルの作成

---
+++ backend/src/common/message.ts
@@ -0,0 +1,9 @@
+//メッセージを管理する定数ファイル
+export const MESSAGE = {
+    AUTH: {
+        LOGIN_SUCCESS: 'ログイン成功',
+        LOGIN_FAILED: 'ユーザーネームまたはパスワードが間違っています',
+        REGIST_SUCCESS: 'アカウントの登録に成功しました',
+        REGIST_FAILED: 'アカウントの作成に失敗しました'
+    },
+};
